3ecfb79e0d4df091ce8b440e90f3cdab8df8e2e5
bing.sun7 <bing.sun.2018@mais.smu.edu.sg>
2020-08-13 17:00:00 +0800

    [dtmc] add support for internal state in sensitivity analysis

--- source/model/lib_models.py
+++ source/model/lib_models.py
@@ -40,3 +40,30 @@ class Model:
             layer.reset()
 
         return output
+
+    def apply_intermediate(self, x):
+        if self.layers == None:
+            return self.__apply_ptmodel(x)
+
+        shape_i = [1, *self.shape[1:]]
+        size_i = np.prod(shape_i)
+
+        len = int(x.size / size_i)
+
+        # only handle single input
+        if len != 1:
+            return None, None
+
+        layer_output = []
+
+        for i in range(len):
+            x_i = x[size_i * i : size_i * (i + 1)].reshape(shape_i)
+            output = x_i
+            for layer in self.layers:
+                output = layer.apply(output)
+                layer_output.append(output)
+
+        for layer in self.layers:
+            layer.reset()
+
+        return output, layer_output
--- source/solver/dtmc_impl.py
+++ source/solver/dtmc_impl.py
@@ -4,6 +4,7 @@ import math
 
 from autograd import grad
 from utils import *
+import time
 
 
 class DTMCImpl():
@@ -21,14 +22,17 @@ class DTMCImpl():
         self.A = []
         self.m = 0
         self.offset = []
+        self.bitshift = []
         self.num_of_path = 0
         self.gen_path = '../debug/gen_path.txt'   #debug purpose
         self.step = 50  #get_new_sample step
         self.sensitive = []
         self.feature = [] # additional feature to analyze
+        self.intermediate_layer = []    # intermediate layer to analyze
         self.under_analyze = []
         self.final = [] #final state
-
+        self.timeout = 20   #timeout value set to 20min
+        self.starttime = time.time()    #start time
 
     def __generate_x(self, shape, lower, upper):
         size = np.prod(shape)
@@ -43,7 +47,7 @@ class DTMCImpl():
             i = i + 1
         return out
 
-    def solve(self, model, assertion, display=None, delta=0.0001, error=0.001, step=50):
+    def solve(self, model, assertion, display=None, delta=0.01, error=0.01, step=50):
         self.model = model
         self.assertion = assertion
         self.display = display
@@ -59,13 +63,24 @@ class DTMCImpl():
         if 'feature' in spec:
             self.feature = np.array(ast.literal_eval(read(spec['feature'])))
 
+        if 'intermediate' in spec:
+            self.intermediate_layer = np.array(ast.literal_eval(read(spec['intermediate'])))
+
+        if 'timeout' in spec:
+            self.timeout = read(spec['timeout']) * 60
 
         # analyze sensitive feature only
         self.under_analyze = np.concatenate((self.feature, self.sensitive), 0)
         print('Sensitive features: {}'. format(self.sensitive))
         print('Other features: {}'.format(self.feature))
+        print('Intermediate layers: {}'.format(self.intermediate_layer))
+        print('Timeout: {}'.format(self.timeout))
+
         # calculate offset to keep each state identical
         self.calc_offset()
+
+        # calculate bitshift for state coding
+        self.calc_bitshift()
 
         #print('Learning DTMC model...')
         self.learn_dtmc_model()
@@ -97,9 +112,43 @@ class DTMCImpl():
 
         size = upper.size
 
-        self.offset.append(256)
+        self.offset.append(1)
         for i in range (1, size):
             self.offset.append(self.offset[i - 1] + upper[i - 1] - lower[i - 1] + 1)
+
+    def calc_bitshift(self):
+        total_analyze = len(self.intermediate_layer) + 4 #start + input layer (sensitive + other feature) + output layer
+
+        for i in range (0, total_analyze):
+            if i == 0:  #start
+                self.bitshift.append(0)
+            elif i == 1: #input sensitive
+                self.bitshift.append(4)
+            elif i == 2: #input other feature
+                # sensitive feature range
+                cal_range = self.model.upper[self.sensitive[0]] - self.model.lower[self.sensitive[0]]
+                sensitive_range = int(cal_range) + 1
+
+                # how many bits needed? how many nibbles
+                nibbles = int(sensitive_range / 16 + 1) # + 1 to handle floating point result
+
+                self.bitshift.append(nibbles * 4 + self.bitshift[i - 1])
+            elif i!= total_analyze - 1:
+                if len(self.feature) == 0:
+                    self.bitshift.append(self.bitshift[i - 1])
+                    continue
+
+                # other feature range
+                cal_range = self.model.upper[self.feature[0]] - self.model.lower[self.feature[0]]
+                feature_range = int(cal_range) + 1
+
+                # how many bits needed? how many nibbles
+                nibbles = int(feature_range / 16 + 1) # + 1 to handle floating point result
+
+                self.bitshift.append(nibbles * 4 + self.bitshift[i - 1])
+            elif i == total_analyze - 1:
+                self.bitshift.append(1)
+
 
     '''
     Update model when add a new trace path
@@ -159,6 +208,7 @@ class DTMCImpl():
     def is_more_sample_needed(self):
         if (self.m == 0) or (self.m == 1):
             return True
+
         for i in range (0, self.m):
             if self.s[i] in self.final:
                 continue
@@ -169,7 +219,29 @@ class DTMCImpl():
                     max_diff = diff
             H = 2.0 / (self.error * self.error) * math.log(2.0 / (self.delta / self.m)) * (0.25 - (max_diff - 2.0 * self.error / 3.0) * (max_diff - 2.0 * self.error / 3.0))
             if self.n_i[i] < H:
+                # timeout?
+                if ((time.time() - self.starttime) > self.timeout):
+                    print('\nTimeout! States that need more sample:')
+                    needs_more = []
+                    for k in range(i, self.m):
+                        if self.s[k] in self.final:
+                            continue
+                        max_diff = 0.0
+                        for p in range(0, self.m):
+                            diff = abs(0.5 - self.n_ij[k][p] / self.n_i[k])
+                            if diff > max_diff:
+                                max_diff = diff
+                        H = 2.0 / (self.error * self.error) * math.log(2.0 / (self.delta / self.m)) * (
+                                0.25 - (max_diff - 2.0 * self.error / 3.0) * (max_diff - 2.0 * self.error / 3.0))
+                        if self.n_i[k] < H:
+                            needs_more.append(self.s[k])
+                            print("0x%016X: %d" % (int(self.s[k]), int(self.n_i[k])))
+                    print("\n")
+
+                    return False
+
                 return True
+
         return False
 
     '''
@@ -181,49 +253,51 @@ class DTMCImpl():
 
         generated = self.step
         out = []
+
         while generated:
             x = self.__generate_x(self.model.shape, lower, upper)
-            y = np.argmax(self.model.apply(x), axis=1)[0]
+            y, layer_op = self.model.apply_intermediate(x)
+            y = np.argmax(y, axis=1)[0]
+            #y = np.argmax(self.model.apply(x), axis=1)[0]
+
+            intermediate_result = []
+            for i in range (0, len(layer_op)):
+                if i in self.intermediate_layer:
+                    layer_sign = np.sign(layer_op[i])
+
+                    # code into one state: each neuron represendted by 2 bits
+                    # TODO: only support positive and non-positive now
+                    layer_state = 0
+                    for j in range (0, len(layer_sign[0])):
+                        layer_state = layer_state | (int((layer_sign[0][j] + 1)) << (2 * j))
+
+                    intermediate_result.append((layer_state << self.bitshift[3]))
+
             path = [self.s0]
-            '''
-            for i in range (0, len(x)):
-                if i in self.feature:
-                    x[i] = x[i] + self.offset[i]
-                    path.append(x[i])
 
-            for i in range (0, len(x)):
-                if i in self.sensitive:
-                    x[i] = x[i] + self.offset[i]
-                    path.append(x[i])
-            '''
-
-            # support only one feature and one sensitive
+            # input feature under analysis
+            # TODO: support only one feature and one sensitive
             to_add = 0
             for i in range (0, len(x)):
                 if i in self.feature:
-                    new = (int(x[i]) + 1) << 16
+                    new = (int(x[i]) + 1) << self.bitshift[2]
                     to_add = to_add | new
                 if i in self.sensitive:
-                    new = (int(x[i]) + 1) << 4
+                    new = (int(x[i]) + 1) << self.bitshift[1]
                     to_add = to_add | new
 
             path.append(to_add)
 
-            path.append((y + self.offset[-1]))
+            # intermediate layer result under analysis
+            # TODO: only support positive and non-positive now
+            for i in range (0, len(intermediate_result)):
+                path.append(intermediate_result[i])
+
+            path.append(int(y + self.offset[-1]))
             self.num_of_path = self.num_of_path + 1
             out.append(path)
             generated = generated - 1
-            '''
-            x = self.__generate_x(model.shape, lower, upper)
-            y = np.argmax(model.apply(x), axis=1)[0]
-            for i in range (0, len(x)):
-                x[i] = x[i] + self.offset[i]
-            path = x.tolist()
-            path.append(y)
-            self.num_of_path = self.num_of_path + 1
-            out.append(path)
-            generated = generated - 1
-            '''
+
         return out
 
 
@@ -254,7 +328,7 @@ class DTMCImpl():
         weight = []
         from_symbol = []
         to_symbol = []
-        for i in range (1, len(self.sensitive) + 2):
+        for i in range (1, (len(self.sensitive)  + len(self.intermediate_layer)) + 2):
             w = []
             _from_symbol = []
             _to_symbol = []
@@ -275,43 +349,60 @@ class DTMCImpl():
 
         # analyze independence fairness
         res = []
-        res.append(weight[len(self.sensitive)])
+        res.append(weight[(len(self.sensitive) + len(self.intermediate_layer))])
         print("Probabilities: \n")
-        print('Sensitive feature {}:'.format(self.sensitive[-1]))
+        #print('Sensitive feature {}:'.format(self.sensitive[-1]))
 
         # print index
         print("transition from:")
-        for item in from_symbol[len(self.sensitive)]:
-            print("0x%08X" % int(self.s[item]))
+        for item in from_symbol[(len(self.sensitive) + len(self.intermediate_layer))]:
+            print("0x%016X" % int(self.s[item]))
         print("transition to:")
-        for item in to_symbol[len(self.sensitive)]:
-            print("0x%08X" % int(self.s[item]))
+        for item in to_symbol[(len(self.sensitive) + len(self.intermediate_layer))]:
+            print("0x%016X" % int(self.s[item]))
 
         #print transformation matrix
-        for item in weight[len(self.sensitive)]:
+        print("\n")
+        for item in weight[(len(self.sensitive) + len(self.intermediate_layer))]:
             print(item)
         #print(np.matrix(weight[len(self.sensitive)]))
         print("\n")
-        for i in range (0, len(self.sensitive)):
-            result = np.matmul(weight[len(self.sensitive) - i - 1], res[i])
+        for i in range (0, (len(self.sensitive) + len(self.intermediate_layer))):
+            result = np.matmul(weight[(len(self.sensitive) + len(self.intermediate_layer)) - i - 1], res[i])
             res.append(result)
-            if i != len(self.sensitive) - 1:
-                print('Sensitive feature {}:'.format(self.sensitive[len(self.sensitive) - i - 2]))
+            '''
+            if i != (len(self.sensitive) + len(self.intermediate_layer)) - 1:
+                print('Sensitive feature {}:'.format(self.sensitive[(len(self.sensitive) + len(self.intermediate_layer)) - i - 2]))
             else:
                 print("Overal probabilities:")
-
+            '''
             # print index
             print("transition from:")
-            for item in from_symbol[len(self.sensitive) - i - 1]:
-                print("0x%08X" % int(self.s[item]))
+            for item in from_symbol[(len(self.sensitive) + len(self.intermediate_layer)) - i - 1]:
+                print("0x%016X" % int(self.s[item]))
             print("transition to:")
-            for item in to_symbol[len(self.sensitive)]:
-                print("0x%08X" % int(self.s[item]))
+            for item in to_symbol[(len(self.sensitive) + len(self.intermediate_layer))]:
+                print("0x%016X" % int(self.s[item]))
 
-            print(np.matrix(result))
+            print("\n")
+            for item in np.matrix(result):
+                print(item)
+
+            #print(np.matrix(result))
             print("\n")
 
+        #print bitshift
+        print("State coding bitshift (0: start, 1: sensitive input feature, 2: other input feature, 3+: intermediate state, last: output):")
+        for item in self.bitshift:
+            print(item)
 
+
+        print("debug message:")
+
+        for i in range (0, len(weight)):
+            print("weight: %d" % i)
+            for item in weight[i]:
+                print(item)
 
         return res
 
